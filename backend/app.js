278c398316d0c6458e6a97824f38bffe9042ec87
Fox Alice <elviratr@yandex.ru>
2023-01-19 09:35:35 +0300

    fix: change connection to MongoDB

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ mongoose.set('strictQuery', true);
 
 dotenv.config();
 
-const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
+const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 
